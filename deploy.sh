19bee9ec296135a9eb424cbb1f6816219dae6aef
pcerypeng <pcerypeng@tencent.com>
2022-09-04 22:16:24 +0800

    feat: vitepress部署

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,8 @@ yarn build
 # 进入生成的文件夹
 cd docs/.vitepress/dist
 
-
+git config --global user.email "1532217526@qq.com"
+git config --global user.name "pcery"
 git init
 git add -A
 git commit -m 'deploy'
