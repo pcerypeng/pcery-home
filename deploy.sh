67479c8d81d4b1a601bea6edd64c7c0c4e004c5e
pcerypeng <pcerypeng@tencent.com>
2022-09-04 22:33:11 +0800

    feat: vitepress部署

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f origin git@github.com:pcerypeng/pcery-home.git main:gh-pages
+git push -f https://github.com/pcerypeng/pcery-home.git main:gh-pages
 
 cd -
